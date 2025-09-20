5dbcf3988eb24b5ad3d1d26ec156cd86d790665a
jujahar <jujahar@gmail.com>
2025-09-19 21:40:01 -0500

    Second Version Week 2 exercise

--- singh_week2.py
+++ singh_week2.py
@@ -2,18 +2,18 @@
 ===========================================
 Title: Assignment 2.1
 Author: Jujahar Singh
-Date: 16 September 2025
+Date: 18 September 2025
 Description: DSC 510 Week 2 - This program calculates the total cost
              of installing fiber optic cable.
 ===========================================
 """
-#Define Constants
+# Define Constant for the per foot cost
 CABLE_COST_PER_FOOT = 0.95
 
 # Welcome the customer
-print("Welcome. Let's start with the cost calculation for fiber optic cable installation.")
+print("Welcome. Let's calculate fiber optic cable installation cost.")
 
-#Get company name from the user
+# Get company name from the user
 company_name = input("Please enter your company's name: ")
 
 # Get the cable length from the user
@@ -25,18 +25,18 @@ try:
 except ValueError:
     print(f"Error: please enter a numeric value for the cable length.")
 
-"""
-Calculate the installation cost by multiplying the per-foot cost 
-by the total cable length
-"""
-installation_cost = cable_length * CABLE_COST_PER_FOOT
+# Calculate the installation cost by multiplying the per-foot cost
+# by the total cable length
+installation_cost = CABLE_COST_PER_FOOT * cable_length
 
+# Referred StackOverflow for the following multi-line print Syntax
+# https://stackoverflow.com/a/55168030
 print(f"""
 ------------------------------
      Installation Receipt     
 ------------------------------
 Company:       {company_name}
-Feet of Cable: {cable_length}
+Cable Length:  {cable_length}
 Total Cost:    ${installation_cost:.2f}
 ------------------------------
 Thank you for your business!
